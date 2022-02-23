d5a3d30f6864a5a9e1b40f5b80bbc940f6539567
E-K8 <ekatplat@gmail.com>
2022-02-23 14:33:08 +0000

    add option for inserting an array of exercises through forEach

--- pages/api/exercises.ts
+++ pages/api/exercises.ts
@@ -2,16 +2,22 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import query from "../../db";
 import NextCors from "nextjs-cors";
+type exercise = {
+  exercise_name: string;
+  sets: number;
+  reps: number;
+  rest_period: string;
+  workout_ref: string;
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   await NextCors(req, res, {
-    
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     origin: "*",
-    optionsSuccessStatus: 200, 
+    optionsSuccessStatus: 200,
   });
 
   if (req.method === "GET") {
@@ -20,18 +26,21 @@ export default async function handler(
   }
 
   if (req.method === "POST") {
-    const el = req.body;
-    const data = await query(
-      "INSERT INTO exercises (exercise_name, sets, reps, rest_period, workout_ref) VALUES($1, $2, $3, $4, $5);",
-      [el.exercise_name, el.sets, el.reps, el.rest_period, el.workout_ref]
-    );
-    res.json(data.rows);
+    const elArray = req.body;
+    // use .forEach to loop through the array of objects
+    elArray.forEach(async (el: exercise) => {
+      const data = await query(
+        "INSERT INTO exercises (exercise_name, sets, reps, rest_period, workout_ref) VALUES($1, $2, $3, $4, $5);",
+        [el.exercise_name, el.sets, el.reps, el.rest_period, el.workout_ref]
+      );
+      res.json(data.rows);
+    });
   }
 
   if (req.method === "DELETE") {
-    await query("DELETE FROM exercises WHERE id = $1;", [
-      req.body.id,
-    ]);
-    res.json({ message: `exercise with the id ${req.body.id} has been deleted`  });
+    await query("DELETE FROM exercises WHERE id = $1;", [req.body.id]);
+    res.json({
+      message: `exercise with the id ${req.body.id} has been deleted`,
+    });
   }
 }
