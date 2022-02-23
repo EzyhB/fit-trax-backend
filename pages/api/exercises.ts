f67afb00b917568a8166103312bb725770dd3e2b
E-K8 <ekatplat@gmail.com>
2022-02-23 11:51:17 +0000

    add DELETE route

--- pages/api/exercises.ts
+++ pages/api/exercises.ts
@@ -19,12 +19,19 @@ export default async function handler(
     res.json(data.rows);
   }
 
-  if (req.method === "POST"){
+  if (req.method === "POST") {
     const el = req.body;
     const data = await query(
       "INSERT INTO exercises (exercise_name, sets, reps, rest_period, workout_ref) VALUES($1, $2, $3, $4, $5);",
       [el.exercise_name, el.sets, el.reps, el.rest_period, el.workout_ref]
     );
     res.json(data.rows);
+  }
+
+  if (req.method === "DELETE") {
+    const data = await query("DELETE FROM exercises WHERE id = $1;", [
+      req.body.id,
+    ]);
+    res.json({ message: "Exercise deleted" });
   }
 }
